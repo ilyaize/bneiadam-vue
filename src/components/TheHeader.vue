7f4ac31a5cec9e22e8bd2e085ba8c9948f9a0b15
Ilya Aizenberg <ilya86berg@gmail.com>
2023-12-20 18:15:36 +0200

    Initial commit

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -77,7 +77,7 @@
         <a
           href="tel:+972544619687"
           class="rounded-full bg-yellow-500 px-7 py-6 text-sm font-semibold text-white shadow-sm hover:bg-yellow-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-400"
-          >Call Now (+972 54-461-9687)</a
+          >Call Now</a
         >
       </div>
     </nav>
