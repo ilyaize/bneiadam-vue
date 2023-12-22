3b83ccf0b0d0f040fab15dcdb794d59a28094ce3
Ilya Aizenberg <ilya86berg@gmail.com>
2023-12-22 08:16:59 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div class="bg-white">
     <main>
-      <TheHeader></TheHeader>
-      <TheContent></TheContent>
-      <TheFooter></TheFooter>
+      <AppHeader></AppHeader>
+      <AppContent></AppContent>
+      <AppFooter></AppFooter>
     </main>
   </div>
 </template>
 
 <script setup>
-import TheHeader from './components/TheHeader.vue'
-import TheContent from './components/TheContent.vue'
-import TheFooter from './components/TheFooter.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppContent from './components/AppContent.vue'
+import AppFooter from './components/AppFooter.vue'
 </script>
 
-<style scoped>
-.dropdown-trigger::after {
-  display: inline-block;
-  margin-left: 0.255em;
-  vertical-align: 0.255em;
-  content: '';
-  border-top: 0.3em solid;
-  border-right: 0.3em solid transparent;
-  border-bottom: 0;
-  border-left: 0.3em solid transparent;
-}
-</style>
+<style scoped></style>
